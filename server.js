1e30a275c0e57fa5438c6c0c7360ba9b097cc449
Chetan <chetansabili@gmail.com>
2026-03-25 22:51:02 +0530

    Fix environment variables - use MONGODB_URI

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const mongoose = require('mongoose');
 require('dotenv').config();
-
-const recipeRoutes = require('./src/routes/recipeRoutes');  //  Add src/
 const connectDB = require('./config/db');
+const recipeRoutes = require('./src/routes/recipeRoutes');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 connectDB();
+
+app.get('/', (req, res) => {
+  res.send('Recipe API is running...');
+});
 
 app.use('/api', recipeRoutes);
 
