52e8171b976aae2f44a698e122ab507f907f0e63
Chetan <chetansabili@gmail.com>
2026-03-25 13:25:39 +0530

    Move server.js to root folder

--- server.js
+++ server.js
@@ -2,29 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const recipeRoutes = require('./routes/recipeRoutes');
+const recipeRoutes = require('./src/routes/recipeRoutes');  //  Add src/
 const connectDB = require('./config/db');
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Connect Database
 connectDB();
 
-// Routes
 app.use('/api', recipeRoutes);
 
-// Error Handlerz`
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).json({ message: 'Server Error', error: err.message });
 });
 
-// Start Server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(` Server running on http://localhost:${PORT}`);
+  console.log(` Server running on port ${PORT}`);
 });
